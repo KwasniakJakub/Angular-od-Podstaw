e763056df790c331887f58f179d3ecd008216629
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-03-26 20:39:03 +0200

    Creating sub-components

--- projekt/src/app/app.module.ts
+++ projekt/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent/*, WelcomeComponent*/]
 })
 export class AppModule { }
