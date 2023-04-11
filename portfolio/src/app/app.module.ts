ba7c8bb677b090ec8b63a650882b684bde891104
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-04-11 18:01:52 +0200

    Roles and component organization

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HerobarComponent } from './components/herobar/herobar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    AboutComponent,
+    NavbarComponent,
+    HerobarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
