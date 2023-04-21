a3a0707b548533b2226a6a79b0d58b3874aca7c2
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-04-21 19:33:28 +0200

    Displaying dynamic collections, section My Projects

--- portfolio/src/app/pages/home/home.component.ts
+++ portfolio/src/app/pages/home/home.component.ts
@@ -3,18 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
- 
+  recentProjects = [
+    {
+      id: 123,
+      name: 'Project ABC',
+      image: 'https://picsum.photos/id/1/200/300',
+    },
+    {
+      id: 234,
+      name: 'Project XYZ',
+      image: 'https://picsum.photos/id/2/200/300',
+    },
+    {
+      id: 345,
+      name: 'Project QWE',
+      image: 'https://picsum.photos/id/3/200/300',
+    },
+  ];
+
   readmore = false;
 
-  value = ''
+  value = '';
 
-  currentTab = ''
+  currentTab = '';
 
-  constructor(){ }
+  constructor() {}
 
-  ngOnInit():void{
-  }
+  ngOnInit(): void {}
 }
