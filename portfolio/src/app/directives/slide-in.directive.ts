2cc4f93ef213790474bf9ea07ffc61c3e9e459fc
KwasniakJakub <jakub.kwasniak@wp.pl>
2023-04-25 12:07:36 +0200

    Animations and behavioral directives

---
+++ portfolio/src/app/directives/slide-in.directive.ts
@@ -0,0 +1,45 @@
+import { Directive, ElementRef, HostBinding, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appSlideIn]',
+  host: {
+    '[class.animate]': 'animate',
+  },
+})
+export class SlideInDirective {
+  // @HostBinding('hidden')
+  // @HostBinding('id')
+  // @HostBinding('style.color')
+
+  //handler: number
+  handler: any
+
+  @Input('appSlideIn')
+  @HostBinding('class')
+  direction = 'left'
+  
+  @HostBinding('class.slideIn')
+  slideIn = true;
+
+  @HostBinding('class.animate')
+  animate = false;
+
+  @Input()
+  delay = 1000;
+
+  constructor(private elem: ElementRef<HTMLElement>) {
+  }
+  
+  
+
+  ngOnInit(): void {
+    this.handler = setTimeout(() => {
+      // this.elem.nativeElement.classList.add('animate');
+      this.animate = true;
+    }, this.delay);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.handler);
+  }
+}
